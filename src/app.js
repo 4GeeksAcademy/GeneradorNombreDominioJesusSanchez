7617d6ed302b3f7ece4aead88baf0dbd041cbe70
jsvasquez356 <157722438+jsvasquez356@users.noreply.github.com>
2025-09-27 04:01:43 +0000

    generador de dominio listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ['El', 'tu'];
+  let adj = ['rincon', 'libro'];
+  let noun = ['vago', 'mapache'];
+                                     
+  for (let primero = 0; primero < pronoun.length; primero++) 
+  for (let segundo = 0; segundo < adj.length; segundo++)
+  for (let tercero = 0; tercero < noun.length; tercero++)
+
+  console.log(pronoun[primero] + adj[segundo] + noun [tercero] + ".com");
+
+
   console.log("Hello Rigo from the console!");
 };
